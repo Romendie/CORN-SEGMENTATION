2245eb3f0c7a3530c0d2221ead4a20cfb7d3b8f3
Romendie <105119465+Romendie@users.noreply.github.com>
2022-10-30 08:14:36 +0000

    func

--- webapp/dist/index.js
+++ webapp/dist/index.js
@@ -3506,4 +3506,24 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ops
 
 /***/ }),
 
-/***/ "./node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/avg_pool.
+/***/ "./node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/avg_pool.js":
+/*!********************************************************************************!*\
+  !*** ./node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/avg_pool.js ***!
+  \********************************************************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ops_avg_pool__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../ops/avg_pool */ \"./node_modules/@tensorflow/tfjs-core/dist/ops/avg_pool.js\");\n/* harmony import */ var _tensor__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../tensor */ \"./node_modules/@tensorflow/tfjs-core/dist/tensor.js\");\n/**\n * @license\n * Copyright 2020 Google LLC. All Rights Reserved.\n * Licensed under the Apache License, Version 2.0 (the \"License\");\n * you may not use this file except in compliance with the License.\n * You may obtain a copy of the License at\n *\n * http://www.apache.org/licenses/LICENSE-2.0\n *\n * Unless required by applicable law or agreed to in writing, software\n * distributed under the License is distributed on an \"AS IS\" BASIS,\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n * See the License for the specific language governing permissions and\n * limitations under the License.\n * =============================================================================\n */\n\n\n_tensor__WEBPACK_IMPORTED_MODULE_1__[\"Tensor\"].prototype.avgPool = function (filterSize, strides, pad, dimRoundingMode) {\n    this.throwIfDisposed();\n    return Object(_ops_avg_pool__WEBPACK_IMPORTED_MODULE_0__[\"avgPool\"])(this, filterSize, strides, pad, dimRoundingMode);\n};\n//# sourceMappingURL=avg_pool.js.map\n\n//# sourceURL=webpack:///./node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/avg_pool.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/batch_to_space_nd.js":
+/*!*****************************************************************************************!*\
+  !*** ./node_modules/@tensorflow/tfjs-core/dist/public/chained_ops/batch_to_space_nd.js ***!
+  \*****************************************************************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ops_batch_to_space_nd__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../ops/batch_to_space_nd */ \"./node_modules/@tensorflow/tfjs-core/dist/ops/batch_to_space_nd.js\");\n/* harmony import */ var _tensor__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../tensor */ \"./node_modules/@tensorflow/tfjs-core/dist/tensor.js\");\n/**\n * @license\n * Copyright 2020 Google LLC. All Rights Reserved.\n * Licensed under the Apache License, Version 2.0 (the \"License\");\n * you may not use this file except in compliance with the License.\n * You may obtain a copy of the License at\n *\n * http://www.apache.org/licenses/LICENSE-2.0\n *\n * Unless required by applicable law or agreed to in writing, software\n * distributed under the License is distributed on an \"AS IS\" BASIS,\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n * See the License for the specific language governing permissions and\n * limitations under the License.\n * =============================================================================\n */\n\n\n_tensor__WEBPACK_IMPORTED_MODULE_1__[\"Tensor\"].prototype.batchToSpaceND = function (blockShape, crops) {\n    this.throwIfDisposed();\n    return
